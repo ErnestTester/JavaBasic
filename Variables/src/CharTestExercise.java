582568611c3a7a2a69b9729c32f98dc933584825
Ernesto Alonso <profile.tester1@outlook.com>
2024-02-03 14:31:53 -0600

    New Class was added this an Example with Chars

---
+++ Variables/src/CharTestExercise.java
@@ -0,0 +1,12 @@
+public class CharTestExercise {
+    public static void main(String[] args) {
+
+        char a;
+        //Initialize the char variable with the value 'p'
+
+        a='p';
+        System.out.println("Value of char is: "+a);
+
+
+    }
+}
