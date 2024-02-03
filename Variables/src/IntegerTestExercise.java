b9430c8ed997ce8c392ef6ec216bf4cda744d27f
Ernesto Alonso <profile.tester1@outlook.com>
2024-02-03 07:46:13 -0600

    New Class was added.

---
+++ Variables/src/IntegerTestExercise.java
@@ -0,0 +1,13 @@
+public class IntegerTestExercise
+{
+    public static void main(String[] args)
+    {
+        //Use the int keyword to declare a integer variable
+        int carSpeed;
+        carSpeed=60;
+        System.out.println("The actual speed for the car is:" + carSpeed);
+        //Car speed was increased with 20, 60 + 20 = 80;
+        carSpeed+=20;
+        System.out.println("Now the speed is:" + carSpeed);
+    }
+}
