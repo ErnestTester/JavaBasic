afc6ac6d1daea2d85b827def56d3d622e0cd7174
Ernesto Alonso <profile.tester1@outlook.com>
2024-02-03 18:42:24 -0600

    add the java class from the gitbach

---
+++ Variables/src/DoubleTestExcercise.java
@@ -0,0 +1,14 @@
+public class DoubleTestExcercise {
+    public static void main(String[] args) {
+        //Use the double keyword to declare decimal variable
+
+        double PI;
+
+        PI=3.1416;
+
+       //Print the value of double variable
+        System.out.println("PI" + PI);
+
+
+    }
+}
