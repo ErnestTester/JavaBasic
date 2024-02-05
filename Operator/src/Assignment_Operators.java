dc8fe97efb7ea716cdc5114dc66b93ab39529336
Ernesto Alonso <profile.tester1@outlook.com>
2024-02-04 23:25:41 -0600

    Added the example for DataType

---
+++ Operator/src/Assignment_Operators.java
@@ -0,0 +1,16 @@
+public class Assignment_Operators {
+
+    static int speed=80; //speed variable gets the value of 80
+    static int distance = 20; // distance variable gets the value 20
+    static int time = 10; // time variable gets the value 10
+    static String name = "JAVA"; // name variable gets the value ToolsQA
+    static boolean isGood = true; // isGood variable gets the value true
+    public static void main(String[] args) {
+
+        System.out.println("Value stored in the speed variable is : " + speed);
+        System.out.println("Value stored in the distance variable is : " + distance);
+        System.out.println("Value stored in the time variable is : " + time);
+        System.out.println("Value stored in the name variable is : " + name);
+        System.out.println("Value stored in the isGood variable is : " + isGood);
+    }
+}
---
+++ Variables/src/Exercise_DataType_2.java
@@ -0,0 +1,8 @@
+public class Exercise_DataType_2 {
+    public static void main(String[] args) {
+        boolean value=true;
+
+        value=false;
+        System.out.println("The value for the Boolean variable is: "+ value);
+    }
+}
