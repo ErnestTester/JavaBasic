1592311b312114521e7959fb3f61916041434760
Ernesto Alonso <profile.tester1@outlook.com>
2024-02-05 15:04:27 -0600

    Added the class for Relational_Operators

---
+++ Operator/src/Relational_Operators.java
@@ -0,0 +1,13 @@
+public class Relational_Operators {
+
+    public static void main(String[] args)
+    {
+        int Ten=10;
+        int Twenty=20;
+        int Thirty=30;
+
+        System.out.println("GREATHER THAN OPERATOR");
+        System.out.println("Ten > Twent=>" + (Ten > Twenty));
+        System.out.println("Twenty > Ten=>" + (Twenty> Ten));
+    }
+}
