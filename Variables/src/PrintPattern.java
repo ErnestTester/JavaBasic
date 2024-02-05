9c8f2dd3f1ee0f3845e8b3168eeee6716346f6f0
Ernesto Alonso <profile.tester1@outlook.com>
2024-02-04 20:14:00 -0600

    Updated the Lines 6 to 7

--- Variables/src/PrintPattern.java
+++ Variables/src/PrintPattern.java
@@ -3,5 +3,8 @@ public class PrintPattern {
     public static void main(String[] args)
     {
         System.out.println("++++++");
+        System.out.println("@@@@@@");
+        System.out.println("******");
+        System.out.println("######");
     }
 }
