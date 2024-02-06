676455a76ca5f8d7a4ef7566ec24c45c13ab3654
Ernesto Alonso <profile.tester1@outlook.com>
2024-02-05 18:01:50 -0600

    Added the class for Relational_Operators

---
+++ Operator/src/Conditional_Operators.java
@@ -0,0 +1,22 @@
+public class Conditional_Operators {
+    public static void main(String[] args)
+    {
+        int Ten=10;
+        int Twenty=20;
+        int Thirty=30;
+
+        boolean bValue;
+        int iValue;
+
+        bValue=(Thirty == Twenty) ? true:false;
+        System.out.println("The boolean value of the variable 'bValue' is: "+ bValue);
+
+        iValue = ((Thirty == Twenty + Ten)) ? 50: 100;
+        System.out.println( "The int Value of the variable iValue is : " + iValue ); //50
+
+        //This is a use of Not Logical Operator
+        iValue = (!(Thirty == Twenty + Ten)) ? 50: 100;
+        System.out.println( "The int Value of the variable iValue is : " + iValue ); //100
+
+    }
+}
---
+++ Operator/src/Logical_Operators.java
@@ -0,0 +1,13 @@
+public class Logical_Operators {
+
+    public static void main(String[] args)
+    {
+        boolean  Output_1=true;
+        boolean Output_2=false;
+
+        System.out.println("Check if both the boolean variables are true: "+ ((Output_1 && Output_2)));
+        System.out.println("Check if one the boolean variables are true: "+ ((Output_1 || Output_2)));
+        System.out.println("Change the state of the Output_1 to false: "+ ((!Output_1)));
+
+    }
+}
