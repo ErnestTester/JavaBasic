0a835852248d068a62f4a5013d8c4ee4e2ddd831
Ernesto Alonso <156559888+ErnestTester@users.noreply.github.com>
2024-02-15 19:00:46 -0600

    Add new classes for Java

---
+++ StringClass/src/StringTestExercise.java
@@ -0,0 +1,13 @@
+public class StringTestExercise
+{
+    public static void main(String[] args)
+    {
+        String sPopularTopic="Selenium Automation Framework";
+        int iLength;
+        iLength=sPopularTopic.length();
+
+        System.out.println("Popular Topic of Test: "+sPopularTopic);
+        System.out.println("Length of the popular Topic: "+iLength);
+
+    }
+}
---
+++ Test/src/Temperature.java
@@ -0,0 +1,26 @@
+ class Temperature
+ {
+
+    public int findNearZero(int[] aTemp)
+    {
+        int min = aTemp[0];
+        int abs;
+        for (int i = 0; i < aTemp.length; i++) {
+
+            if (min >= Math.abs(aTemp[i]))
+            {
+                min = aTemp[i];
+            }
+
+
+        }
+        return  min;
+
+    }
+        public static void main (String[]args)
+        {
+            int aTemp[] = {-3,-7, -10, 13, 8, 3};
+            Temperature obj=new Temperature();
+            System.out.printf("Min value is:" + Math.abs(obj.findNearZero(aTemp)));
+        }
+    }
